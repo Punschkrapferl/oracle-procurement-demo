3d67be205a2e097996e5c874a8441938d68cc4d2
Punschkrapferl <waldsa01@thu.de>
2026-04-13 14:03:57 +0200

    Initial commit: bootstrap Spring Boot Oracle project

---
+++ src/main/java/com/example/oracleprocurementdemo/OracleProcurementDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oracleprocurementdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleProcurementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleProcurementDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oracleprocurementdemo/OracleProcurementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oracleprocurementdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleProcurementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
